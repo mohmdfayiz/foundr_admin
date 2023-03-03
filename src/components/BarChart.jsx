de85cd0752470017f92cb8c17482e99e36f6be81
Mohammed fayis <90344973+mohmd-fayis@users.noreply.github.com>
2023-03-03 22:02:26 +0530

    bug fixes

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { Bar } from 'react-chartjs-2'
-import {Chart} from 'chart.js/auto'
+// import React from 'react'
+// import { Bar } from 'react-chartjs-2'
+// import {Chart} from 'chart.js/auto'
 
-function BarChart({chartData}) {
-  return (
-    <Bar data={chartData}/>
-  )
-}
+// function BarChart({chartData}) {
+//   return (
+//     <Bar data={chartData}/>
+//   )
+// }
 
-export default BarChart
+// export default BarChart
